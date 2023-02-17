fb6cdb21393136bc5a8da73bd24d10d9a4ae69a7
Toss <tossdesu@gmail.com>
2023-02-17 14:23:23 +0700

    Create recyclerview item layout, modify activity_main.xml

--- app/src/main/java/com/tossdesu/recipestest/presentation/MainActivity.kt
+++ app/src/main/java/com/tossdesu/recipestest/presentation/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.tossdesu.recipestest
+package com.tossdesu.recipestest.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.tossdesu.recipestest.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
