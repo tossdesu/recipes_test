87863783a41ead101a6dddf77a26cae7ff3da7a5
Toss <tossdesu@gmail.com>
2023-02-17 11:45:29 +0700

    Initial commit

---
+++ app/src/main/java/com/tossdesu/recipestest/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.tossdesu.recipestest
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
